e27633f3b9d597f15a100c1ac584339c5a1327ca
Daoyuan Yang <gy199445@gmail.com>
2017-10-13 13:33:31 +0200

    plot mean value

--- sensorNoiseAnalysis.m
+++ sensorNoiseAnalysis.m
@@ -15,14 +15,17 @@ magMean = mean(mag,2);
 %plot hisogram to check the noise
 figure(1)
 for i = 1:n
-    subplot(3,3,1+(i-1)*3);
-    histogram(acc(i,:)-accMean(i),'Normalization','pdf')
+    subplot(3,3,1+(i-1)*3);hold on;
+    histogram(acc(i,:),'Normalization','pdf')
+    scatter(accMean(i),0,'ro')
     title(sprintf('acc_{%d}',i))
-    subplot(3,3,2+(i-1)*3);
-    histogram(mag(i,:)-magMean(i),'Normalization','pdf')
+    subplot(3,3,2+(i-1)*3);hold on;
+    histogram(mag(i,:),'Normalization','pdf')
+    scatter(magMean(i),0,'ro')
     title(sprintf('mag_{%d}',i))
-    subplot(3,3,3+(i-1)*3);
-    histogram(gyr(i,:)-gyrMean(i),'Normalization','pdf')
+    subplot(3,3,3+(i-1)*3);hold on;
+    histogram(gyr(i,:),'Normalization','pdf')
+    scatter(gyrMean(i),0,'ro')
     title(sprintf('gyr_{%d}',i))
 end
 noiseParameters = struct('accMean',accMean,'accCov',cov(acc'),...

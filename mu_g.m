bacd52c0c77a5ff31baab7ead558afb8b596d2b1
Daoyuan Yang <gy199445@gmail.com>
2017-10-17 19:06:11 +0200

    fix bug

--- mu_g.m
+++ mu_g.m
@@ -4,10 +4,10 @@ function [xUpdate, PUpdate] = mu_g(xPredict, PPredict, yacc, Ra, g0)
 %   Ra: measurement noise conv
 H = Qq(xPredict);
 [Q0, Q1, Q2, Q3] = dQqdq(xPredict);
-H_J = [Q0*g0, Q1*g0, Q2*g0, Q3*g0];
+H_J = [Q0'*g0, Q1'*g0, Q2'*g0, Q3'*g0];
 S = H_J*PPredict*H_J' + Ra;
 K = PPredict*H_J'*inv(S);
-xUpdate = xPredict + K*(yacc - H*g0);
+xUpdate = xPredict + K*(yacc - H'*g0);
 PUpdate = PPredict - K*S*K';
 [xUpdate, PUpdate] = mu_normalizeQ(xUpdate, PUpdate);   %normalize                                
 end
